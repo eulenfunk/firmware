afbed66f0085980998a60267e1a276e354cceb5f
plumpudding <9lum9u66ing@gmail.com>
2015-04-13 20:07:37 +0200

    fix build script

--- build.sh
+++ build.sh
@@ -1,18 +1,17 @@
 rm -rf ../images/*
 
-for f in site.conf.*
+for f in bcd lln bgl ode
 do
 	rm site.conf
-	cp $f site.conf
+	cp site.conf.$f site.conf
 	cd ..
 	make clean
 	make -j20 GLUON_BRANCH=stable
 	make manifest GLUON_BRANCH=stable
 	contrib/sign.sh secret.key images/sysupgrade/stable.manifest
-	rm -rf images/${f##*.}
-	mkdir images/{f##*.}
-	mv images/factory images/{f##*.}/
-	mv images/sysupgrade images/{f##*.}/
+	mkdir images/$f/stable
+	mv images/factory images/$f/stable/
+	mv images/sysupgrade images/$f/stable/
 	cd site
 done
 
