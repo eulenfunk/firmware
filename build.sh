7080a0d46145075197151d189d47450ea0098c66
PetaByteBoy <petabyteboy@gmail.com>
2015-05-17 21:06:56 +0200

    edit build script

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ rm -rf ../images/*
 for f in bcd bgl kut lln ode ovr rrh
 do
 	cd ..
-	make clean GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=site/$f GLUON_IMAGEDIR=images/$f
-	make -j20 GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=site/$f GLUON_IMAGEDIR=images/$f
-	make manifest GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=site/$f GLUON_IMAGEDIR=images/$f
-	#contrib/sign.sh secret.key images/sysupgrade/stable.manifest
+	make clean GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=$PWD/site/$f GLUON_IMAGEDIR=$PWD/images/$f
+	make -j20 GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=$PWD/site/$f GLUON_IMAGEDIR=$PWD/images/$f
+	make manifest GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=$PWD/site/$f GLUON_IMAGEDIR=$PWD/images/$f
+	#contrib/sign.sh secret.key $PWD/images/sysupgrade/stable.manifest
 	cd site
 done
