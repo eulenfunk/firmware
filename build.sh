47b96847d22a8216fee716007abd791c1855ff75
PetaByteBoy <petabyteboy@gmail.com>
2015-05-17 22:26:49 +0200

    change build script (2

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-rm -rf ../images/*
+cd ..
+rm -rf images
+
+make clean GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=$PWD/site/bcd GLUON_IMAGEDIR=$PWD/images/$f/stable
 
 for f in bcd bgl kut lln ode ovr rrh
 do
-	cd ..
-	make clean GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=$PWD/site/$f GLUON_IMAGEDIR=$PWD/images/$f
-	make -j20 GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=$PWD/site/$f GLUON_IMAGEDIR=$PWD/images/$f
-	make manifest GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=$PWD/site/$f GLUON_IMAGEDIR=$PWD/images/$f
+	make -j20 GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=$PWD/site/$f GLUON_IMAGEDIR=$PWD/images/$f/stable
+	make manifest GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=$PWD/site/$f GLUON_IMAGEDIR=$PWD/images/$f/stable
 	#contrib/sign.sh secret.key $PWD/images/sysupgrade/stable.manifest
-	cd site
 done
