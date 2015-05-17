7b69c1677d809cbf1b314a28a13e259f5e065f19
PetaByteBoy <petabyteboy@gmail.com>
2015-05-17 21:40:04 +0300

    new script

--- build.sh
+++ build.sh
@@ -2,17 +2,10 @@ rm -rf ../images/*
 
 for f in bcd bgl kut lln ode ovr rrh
 do
-	rm site.conf
-	rm -rf i18n
-	cp site.conf.$f site.conf
-	cp i18n.$f i18n
 	cd ..
-	make clean GLUON_TARGET=ar71xx-generic
-	make -j20 GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic
-	make manifest GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic
-	contrib/sign.sh secret.key images/sysupgrade/stable.manifest
-	mkdir -p images/$f/stable
-	mv images/factory images/$f/stable/
-	mv images/sysupgrade images/$f/stable/
+	make clean GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=site/$f GLUON_IMAGEDIR=images/$f
+	make -j20 GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=site/$f GLUON_IMAGEDIR=images/$f
+	make manifest GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=site/$f GLUON_IMAGEDIR=images/$f
+	#contrib/sign.sh secret.key images/sysupgrade/stable.manifest
 	cd site
 done
