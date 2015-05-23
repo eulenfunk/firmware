7894c2aa14ebbefe0dc822deb119da2b76b52eab
PetaByteBoy <petabyteboy@gmail.com>
2015-05-23 10:41:53 +0200

    0.7.0

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 cd ..
 rm -rf oldimages
 mv images oldimages
-make clean GLUON_TARGET=x86-kvm_guest GLUON_SITEDIR=$PWD/site/bcd GLUON_IMAGEDIR=$PWD/images/$f/stable
+make clean GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=$PWD/site/bcd GLUON_IMAGEDIR=$PWD/images/$f/stable
 
 for f in bcd bgl kut lln ode ovr rrh
 do
-	make -j20 GLUON_BRANCH=stable GLUON_TARGET=x86-kvm_guest GLUON_SITEDIR=$PWD/site/$f GLUON_IMAGEDIR=$PWD/images/$f/stable
-	make manifest GLUON_BRANCH=stable GLUON_TARGET=x86-kvm_guest GLUON_SITEDIR=$PWD/site/$f GLUON_IMAGEDIR=$PWD/images/$f/stable
+	make -j20 GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=$PWD/site/$f GLUON_IMAGEDIR=$PWD/images/$f/stable
+	make manifest GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic GLUON_SITEDIR=$PWD/site/$f GLUON_IMAGEDIR=$PWD/images/$f/stable
 	#contrib/sign.sh secret.key $PWD/images/sysupgrade/stable.manifest
 done
