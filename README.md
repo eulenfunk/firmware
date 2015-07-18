e8102b28e7918d070beb6017739e237249fe9d98
PetaByteBoy <petabyteboy@gmail.com>
2015-07-18 12:43:44 +0200

    update readme and scripts

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Community specific configurations for gluon firmwares in gl.wupper
 
 first run init.sh
-then change the site configs in basearch/basesitecode to fit your needs
-edit build.sh and add your communities
+then create the base site configs in basesites/ or change the existing ones to fit your needs
+edit build.sh and select your your communities
 finally run build.sh
--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+##########################################################
+
+#sites and architectures to build
+
+#SITE="gek"
+SITE="bcd bgl cgr kut lgn lln ode ovr rrh"
+ARCHS="ar71xx-generic ar71xx-nand mpc85xx-generic x86-kvm_guest x86-generic"
+
+##########################################################
+
 SCRIPTHOME=$(pwd)
 
 function mksite {
+	#ARGS: site_code name homepage base SSID
 	echo $1
-	#site_code name homepage base SSID
 	cp -r $SCRIPTHOME/basesites/$4/ $SCRIPTHOME/sites/$1
 	sed -e "s/basesitecode/$1/g" -i $SCRIPTHOME/sites/$1/site.conf
 	sed -e "s/basename/$2/g" -i $SCRIPTHOME/sites/$1/site.conf
@@ -17,8 +27,11 @@ function mksite {
 
 function sites {
 	echo -generating sites-
+	#clean generated sites dir
 	rm -rf $SCRIPTHOME/sites
 	mkdir -p $SCRIPTHOME/sites
+
+	#call mksite function for each site to copy the corresponding base site and replace values
 	mksite bcd Burscheid freifunk-burscheid.de gl Freifunk-Burscheid
 	mksite bgl "Bergisch Gladbach" freifunk-bergisch-gladbach.de gl Freifunk
 	mksite cgr Rechtsrheinisch-Köln freifunk-gl.de gl Freifunk
@@ -28,18 +41,22 @@ function sites {
 	mksite ode Odenthal "odenthal.de\/freifunk.html" gl Freifunk
 	mksite ovr Overath freifunk-gl.de gl Freifunk
 	mksite rrh Rösrath freifunk-gl.de gl Freifunk
+
+	#just copy gek basesite as there is nothing to be replaced
 	cp -r $SCRIPTHOME/basesites/gek/ $SCRIPTHOME/sites/gek
+
 	echo done.
 }
 
 function images {
 	cd $SCRIPTHOME/build
+	#clean images dirs
 	rm -rf $SCRIPTHOME/oldimages
 	mv $SCRIPTHOME/images $SCRIPTHOME/oldimages
-#	for f in gek
-	for f in bcd bgl cgr kut lgn lln ode ovr rrh
+
+	for f in $SITES
 	do
-		for g in ar71xx-generic ar71xx-nand mpc85xx-generic x86-kvm_guest x86-generic
+		for g in $ARCHS
 		do
 			echo -building $f $g-
 			ARGS="GLUON_TARGET=$g GLUON_SITEDIR=$SCRIPTHOME/sites/$f GLUON_IMAGEDIR=$SCRIPTHOME/images/$f/stable GLUON_BRANCH=stable"
@@ -48,7 +65,7 @@ function images {
 			make -j20 BROKEN=1 $ARGS
 		done
 		make manifest $ARGS
-		contrib/sign.sh secret.key $SCRIPTHOME/images/$f/stable/sysupgrade/stable.manifest
+		contrib/sign.sh $SCRIPTHOME/secret.key $SCRIPTHOME/images/$f/stable/sysupgrade/stable.manifest
 	done
 }
 
