fa45fb8393e27751188b07ead166b233e56a7f74
Andreas Dorfer <adorferen@gmail.com>
2016-02-03 17:01:49 +0100

    fixed radiochannel persistance

--- templates/dus/prepare.sh
+++ templates/dus/prepare.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
 git apply $(dirname $0)/keepradiochannel.diff
-echo "CONFIG_PATA_ATIIXP=y" >> $(dirname $0)/openwrt/target/linux/x86/generic/config-default
+echo "CONFIG_PATA_ATIIXP=y" >> /home/build/firmware/gluon/openwrt/target/linux/x86/generic/config-default
 
 
