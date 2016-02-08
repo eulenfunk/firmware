bbe2a5915acd6952c9943c56f86e39333280e201
Milan Paessler <me@petabyteboy.de>
2016-02-08 21:08:29 +0100

    pata-fix is alread integrade

--- templates/dus/prepare.sh
+++ templates/dus/prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git apply $(dirname $0)/keepradiochannel.diff
-echo "CONFIG_PATA_ATIIXP=y" >> openwrt/target/linux/x86/generic/config-default
+# echo "CONFIG_PATA_ATIIXP=y" >> openwrt/target/linux/x86/generic/config-default
 
 
