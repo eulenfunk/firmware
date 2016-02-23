0e946a0389c3066f5bdaf99f52df43dd115abac5
Milan Paessler <me@petabyteboy.de>
2016-02-23 03:36:45 +0100

    gek hinzugefuegt

---
+++ templates/gek/site.mk
@@ -0,0 +1,42 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	gluon-weeklyreboot \
+	gluon-wificheck \
+	gluon-ssid-changer \
+	gluon-aptimeclock \
+	gluon-vpnlimittimeclock \
+	gluon-txpowerfix \
+	haveged \
+	iptables \
+	iwinfo \
+	ffho-wifi-blackout-workaround \
+	gluon-ebtables-filter-roguenets \
+        ffffm-keep-radio-channel 
+#        gluon-banner
+
+DEFAULT_GLUON_RELEASE := $(shell date '+%Y%m%d%H')-exp
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+GLUON_LANGS ?= de en 
