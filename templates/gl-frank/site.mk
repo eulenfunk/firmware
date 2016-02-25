394b2b33ae90ec25e9787fc0679814005648c83a
Milan Pässler <me@petabyteboy.de>
2016-02-25 18:24:21 +0100

    add gl-frank and x86 packages

--- templates/gl-frank/site.mk
+++ templates/gl-frank/site.mk
@@ -1,6 +1,7 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
 	gluon-respondd \
+	gluon-authorized-keys \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
