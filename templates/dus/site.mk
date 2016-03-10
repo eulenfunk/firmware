df6d100edf2a6798fc1726a876add3012ea34811
Adorfer <adorferen@gmail.com>
2016-03-10 22:51:43 +0100

    besser mit linkcheck

--- templates/dus/site.mk
+++ templates/dus/site.mk
@@ -20,7 +20,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-setup-mode \
 	gluon-status-page \
 	gluon-weeklyreboot \
-	gluon-wificheck \
+	gluon-linkcheck \
 	gluon-ssid-changer \
 	gluon-aptimeclock \
 	gluon-vpnlimittimeclock \
