8ea82be1f983f2c053eb9f4907c985ae844970f4
Andreas Dorfer <adorferen@gmail.com>
2016-04-06 03:19:20 +0200

    first attempt for l2tp dus

---
+++ templates/dus-l2tp/site.mk
@@ -0,0 +1,43 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-tunneldigger \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-tunneldigger-watchdog \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	gluon-weeklyreboot \
+	gluon-ssid-changer \
+	gluon-aptimeclock \
+	gluon-vpnlimittimeclock \
+	gluon-txpowerfix \
+	haveged \
+	iptables \
+	iwinfo \
+	gluon-ebtables-filter-roguenets \
+        ffffm-keep-radio-channel \
+        gluon-banner \
+	gluon-migrade-vpn \
+	gluon-linkcheck
+
+DEFAULT_GLUON_RELEASE := SBRANCH
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+GLUON_LANGS ?= de en 
