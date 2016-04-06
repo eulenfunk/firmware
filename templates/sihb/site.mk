4a8a2891b30018898cf5dd37ccd9235f05a8928f
Andreas Dorfer <adorferen@gmail.com>
2016-04-06 02:15:36 +0200

    dus on 2016.1.3 master und hilchenbach-test

---
+++ templates/sihb/site.mk
@@ -0,0 +1,41 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-alfred \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	haveged \
+	iptables \
+	iwinfo \
+	ffffm-keep-radio-channel \
+	ffho-wifi-blackout-workaround \
+	gluon-weeklyreboot \
+	gluon-ssid-changer \
+	gluon-aptimeclock \
+	gluon-vpnlimittimeclock \
+	gluon-txpowerfix \
+	gluon-banner
+
+DEFAULT_GLUON_RELEASE := SBRANCH
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+GLUON_LANGS ?= de 
