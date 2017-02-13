80f723f86484d2f7910e0a5b807a8ab62c5e5ce8
Milan Paessler <me@petabyteboy.de>
2017-02-13 19:01:14 +0100

    ueberlfuessiges

--- templates/dusl2tp/site.mk
+++ templates/dusl2tp/site.mk
@@ -1,7 +1,6 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
 	gluon-respondd \
-	gluon-next-node \
 	respondd-module-airtime \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
@@ -35,49 +34,6 @@ GLUON_SITE_PACKAGES := \
 	gluon-migrate-vpn \
 	gluon-linkcheck
 
-
-USB_BASIC := \
-	kmod-usb-core \
-	kmod-usb2 \
-	kmod-usb-hid
-
-USB_NIC := \
-	kmod-usb-net \
-	kmod-usb-net-asix \
-	kmod-usb-net-rtl8150 \
-	kmod-usb-net-rtl8152 \
-	kmod-usb-net-dm9601-ether
-
-USB_WIFI := \
-	kmod-rtl8192cu
-
-ifeq ($(GLUON_TARGET),x86-generic)
-	GLUON_SITE_PACKAGES += \
-		$(USB_BASIC) \
-		kmod-usb-ohci-pci \
-		$(USB_NIC)
-endif
-
-ifeq ($(GLUON_TARGET),x86-64)
-	GLUON_SITE_PACKAGES += \
-		$(USB_BASIC) \
-		$(USB_NIC) \
-		kmod-igb #APU2
-endif
-
-ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
-	GLUON_SITE_PACKAGES += \
-		$(USB_BASIC) \
-		$(USB_NIC) \
-		$(USB_WIFI)
-endif
-
-ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
-	GLUON_SITE_PACKAGES += \
-		$(USB_BASIC) \
-		$(USB_NIC) \
-		$(USB_WIFI)
-endif
 
 
 DEFAULT_GLUON_RELEASE := SBRANCH
