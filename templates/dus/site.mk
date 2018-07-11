4f59e10f54ea5bc5154d0d520b3d2d057bc4b305
rubo77 <github@r.z11.de>
2018-07-11 17:16:37 +0200

    adapt modules, site.conf and site.mk to 2018.1.x
    see https://github.com/freifunk-gluon/gluon/blob/master/docs/releases/v2018.1.rst#igmpmld-segmentation

--- templates/dus/site.mk
+++ templates/dus/site.mk
@@ -1,39 +1,33 @@
 # site.mk for Freifunk Duesseldorf-Flingern
 
-# standard gluon:
-GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-15 \
-	gluon-respondd \
-	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-contact-info \
-	gluon-config-mode-core \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-ebtables \
-	gluon-ebtables-segment-mld \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-web-admin \
-	gluon-web-autoupdater \
-	gluon-web-network \
-	gluon-web-wifi-config \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-setup-mode \
-	gluon-status-page
+GLUON_FEATURES := \
+	web-wizard
+	web-advanced
+	mesh-batman-adv-15 \
+	respondd \
+	autoupdater \
+	ebtables \
+	ebtables-limit-arp \
+	radv-filterd \
+	ebtables-filter-multicast \
+	ebtables-filter-ra-dhcp \
+	mesh-vpn-fastd \
+	radvd \
+	status-page
+
 # eulenfunk:
-GLUON_SITE_PACKAGES += \
+GLUON_SITE_PACKAGES := \
 	gluon-ebtables-limit-arp \
 	gluon-weeklyreboot \
-	gluon-ssid-changer \
 	gluon-hotfix \
 	gluon-quickfix \
 	gluon-txpowerfix \
 	eulenfunk-dns-cache \
 	gluon-banner \
 	gluon-linkcheck
+# ssid_changer (Freifunk Nord):
+GLUON_SITE_PACKAGES += \
+	gluon-ssid-changer 
 # ffho:
 GLUON_SITE_PACKAGES += \
 	ffho-ath9k-blackout-workaround
