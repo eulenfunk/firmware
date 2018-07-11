3fdf49471ba1ab147a5e2757d1e2388879bd35c9
rubo77 <github@r.z11.de>
2018-07-11 12:00:43 +0200

    add validate-script to determin the source of packages and add comments in site.conf to explain packages and sections

--- templates/dus/site.mk
+++ templates/dus/site.mk
@@ -1,3 +1,6 @@
+# site.mk for Freifunk Duesseldorf-Flingern
+
+# standard gluon:
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
 	gluon-respondd \
@@ -10,9 +13,13 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables \
 	gluon-ebtables-segment-mld \
-        gluon-ebtables-filter-multicast \
-        gluon-ebtables-filter-ra-dhcp \
-        gluon-ebtables-limit-arp \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp
+# eulenfunk:
+GLUON_SITE_PACKAGES += \
+	gluon-ebtables-limit-arp
+# standard gluon:
+GLUON_SITE_PACKAGES += \
 	gluon-next-node \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
@@ -21,23 +28,37 @@ GLUON_SITE_PACKAGES := \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-setup-mode \
-	gluon-status-page \
+	gluon-status-page
+# eulenfunk:
+GLUON_SITE_PACKAGES += \
 	gluon-weeklyreboot \
 	gluon-ssid-changer \
 	gluon-hotfix \
 	gluon-quickfix \
-	gluon-txpowerfix \
-	ffho-ath9k-blackout-workaround \
+	gluon-txpowerfix
+# ffho:
+GLUON_SITE_PACKAGES += \
+	ffho-ath9k-blackout-workaround
+# openwrt:
+GLUON_SITE_PACKAGES += \
 	haveged \
 	iptables \
-	iwinfo \
+	iwinfo
+# ffrl_packages:
+GLUON_SITE_PACKAGES += \
 	gluon-ebtables-filter-roguenets \
-        ffffm-keep-radio-channel \
- 	ffffm-additional-wifi-json-info \
+	ffffm-keep-radio-channel \
+	ffffm-additional-wifi-json-info
+# eulenfunk:
+GLUON_SITE_PACKAGES += \
 	eulenfunk-dns-cache \
-        gluon-banner \
-	gluon-linkcheck \
-	gluon-config-mode-ppa \
+	gluon-banner \
+	gluon-linkcheck
+# ffki:
+GLUON_SITE_PACKAGES += \
+	gluon-config-mode-ppa
+# openwrt:
+GLUON_SITE_PACKAGES += \
 	socat
 
 DEFAULT_GLUON_RELEASE := SBRANCH
