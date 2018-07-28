fdc7bd323943d04c08357d164eff2f383a63c09d
Adorfer <adorferen@gmail.com>
2018-07-29 01:52:24 +0200

    updated modules (added ffm-buttons, revmoved ffm-wifiinfo)

--- templates/dus/site.mk
+++ templates/dus/site.mk
@@ -40,7 +40,8 @@ GLUON_SITE_PACKAGES := \
 # ev. kann man ffffm-additional-wifi-json-info teilweise durch standard gluon respondd-module-airtime ersetzen
 GLUON_SITE_PACKAGES += \
 	ffffm-keep-radio-channel \
-	ffffm-additional-wifi-json-info
+        ffffm-luci-switchconfig \
+	ffffm-button-bind 
 
 # ffki:
 GLUON_SITE_PACKAGES += \
