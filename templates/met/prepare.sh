ea0e30cf8a39052cd9e68259dcafa7fc3cd15c25
Adorfer <adorferen@gmail.com>
2018-09-10 23:12:25 +0200

    changed patch process

--- templates/met/prepare.sh
+++ templates/met/prepare.sh
@@ -2,7 +2,6 @@
 # git apply $(dirname $0)/keepradiochannel.diff
 # echo "CONFIG_PATA_ATIIXP=y" >> openwrt/target/linux/x86/generic/config-default
  
-curl https://gist.githubusercontent.com/Adorfer/3c60347cf8599beed6c0f2d2124e95f5/raw/df1762934f99397b386af633636af37507e32982/sysupgrade  > ./lede/package/base-files/files/sbin/sysupgrade
+cp ../patches/sysupgrade ./lede/package/base-files/files/sbin/sysupgrade
 chmod +x ./lede/package/base-files/files/sbin/sysupgrade
-
 
