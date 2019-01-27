9a89f39bde3d0f94c2b4724abf8388e71e9f02e5
plaste <stephan@neanderfunk.de>
2019-01-27 19:18:20 +0100

    configuration changes ffnef-multidomain

--- templates/ffnef-multidomain-key/site.mk
+++ templates/ffnef-multidomain-key/site.mk
@@ -1,4 +1,6 @@
-# site.mk for Freifunk im Neanderland - gluon 2018.1.x
+# site.mk for Freifunk im Neanderland - gluon 2018.2.x
+
+GLUON_MULTIDOMAIN=1
 
 # for feature packs see https://github.com/freifunk-gluon/gluon/blob/v2018.2.x/package/features
 GLUON_FEATURES := \
