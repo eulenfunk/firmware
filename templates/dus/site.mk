b7898e4cfc3ca80b823073d49cdf82de1d61fae1
Adorfer <adorferen@gmail.com>
2019-06-21 15:33:42 +0200

    more zram

--- templates/dus/site.mk
+++ templates/dus/site.mk
@@ -68,6 +68,11 @@ ifeq ($(GLUON_TARGET),ar71xx-tiny)
 GLUON_SITE_PACKAGES += zram-swap
 endif
 
+ifeq ($(GLUON_TARGET),ar71xx-generic)
+GLUON_SITE_PACKAGES += zram-swap
+endif
+
+
 DEFAULT_GLUON_RELEASE := SBRANCH
 
 # Allow overriding the release number from the command line
