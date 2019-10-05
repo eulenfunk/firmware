aba4584fddb40439e089f8b78af905ac9c17546b
Adorfer <adorferen@gmail.com>
2019-10-05 04:42:19 +0200

    remove type

--- templates/dusl2tpkey/site.mk
+++ templates/dusl2tpkey/site.mk
@@ -30,7 +30,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-geo-location-osm \
 	gluon-authorized-keys \
         eulenfunk-migrate-updatebranch \
-        eulenfunk-ath9k-blackout-workaround \	
+        eulenfunk-ath9k-blackout-workaround
 
 # PROBLEM:
 # ev. macht dieses paket:
