4941aa15e41d04127dc10ccd5515eca908804550
Adorfer <adorferen@gmail.com>
2020-06-18 21:32:05 +0200

    renamed

---
+++ templates/01_vel-key/prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# git apply $(dirname $0)/keepradiochannel.diff
+# echo "CONFIG_PATA_ATIIXP=y" >> openwrt/target/linux/x86/generic/config-default
+ 
+#cp ../patches/sysupgrade ./openwrt/package/base-files/files/sbin/sysupgrade
+#chmod +x ./openwrt/package/base-files/files/sbin/sysupgrade
+
+cd ../gluon ; ../patches/fix-respondd-rsk.sh  # change respondd listener address to gluon 2016.x value
+cd ../gluon ; ../patches/fix-DIR615c1-imagetoobig.sh # remove DIR615C1 beeing too big
