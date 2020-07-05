95cff50bd87eb90289b1edbd50edb1d8bec2b229
Adorfer <adorferen@gmail.com>
2020-07-06 00:36:39 +0200

    next testrun hlg

--- patches/ignore-preservechannels-for-outdoormode.sh
+++ patches/ignore-preservechannels-for-outdoormode.sh
@@ -4,3 +4,5 @@ patchfile="../patches/ignore-preservechannels-for-outdoormode.patch"
 if ! patch -R -p1 -s -f --ignore-whitespace --dry-run <$patchfile; then
   patch -p1 --ignore-whitespace <$patchfile
 fi
+grep 'force_channel_outdoor'  package/gluon-core/luasrc/lib/gluon/upgrade/200-wireless
+
