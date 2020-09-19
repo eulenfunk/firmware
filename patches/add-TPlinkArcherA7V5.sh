a251f78caf5cf1b98f6654641324390c67145e84
Adorfer <adorferen@gmail.com>
2020-09-19 21:49:10 +0200

    adding archer-a7-v5 for testing

---
+++ patches/add-TPlinkArcherA7V5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo $PWD 
+
+patchfile="../patches/add-TPlinkArcherA7V5.patch"
+if ! patch -R -p1 -s -f --ignore-whitespace --dry-run <$patchfile; then
+  patch -p1 --ignore-whitespace <$patchfile
+fi
+grep 'archer-a7-v5'  targets/ath79-generic
