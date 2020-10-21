54d3e2b5c11b680a7ec568d6f4adc860ab20ebe0
Adorfer <adorferen@gmail.com>
2020-10-21 10:22:01 +0200

    updated for 2020.2.x

--- patches/backport-gluon2020-02.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo $PWD 
-
-patchfile="../patches/backport-gluon2020-02.patch"
-if ! patch -R -p1 -s -f --ignore-whitespace --dry-run <$patchfile; then
-  patch -p1 --ignore-whitespace <$patchfile
-fi
-grep 'archer-c6-v2'  targets/ath79-generic
