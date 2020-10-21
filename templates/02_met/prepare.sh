34e03a173a48550b806c922a1db9979889315574
Adorfer <adorferen@gmail.com>
2020-10-21 08:17:11 +0200

    upated patches, backported new targets from 2020.2, updated to fastd19

--- templates/02_met/prepare.sh
+++ templates/02_met/prepare.sh
@@ -6,7 +6,7 @@
 #chmod +x ./openwrt/package/base-files/files/sbin/sysupgrade
 
 cd ../gluon ; ../patches/fix-respondd-rsk.sh  # change respondd listener address to gluon 2016.x value
-cd ../gluon ; ../patches/fix-DIR615c1-imagetoobig.sh # remove DIR615C1 beeing too big
 cd ../gluon ; ../patches/backport-gluon2020-02.sh # backport packages vom Gluon2020.2.x
+cd ../gluon ; ../patches/fix-DIR615c1-imagetoobig.sh # remove DIR615C1 beeing too big
 cd ../gluon ; ../patches/add-TPlinkArcherA7V5.sh # adding TP-Link Archer A7-V5
 cd ../gluon ; ../patches/ignore-preservechannels-for-outdoormode.sh # correct handling of outdoor-devices (enable auto-channel)
