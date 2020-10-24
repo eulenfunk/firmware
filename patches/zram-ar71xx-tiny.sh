85ab5b20216604dba4ff5f03b669f51f6f916b1d
Adorfer <adorferen@gmail.com>
2020-10-24 14:45:03 +0200

    modified zram-patches

--- patches/zram-ar71xx-tiny.sh
+++ patches/zram-ar71xx-tiny.sh
@@ -4,5 +4,12 @@ patchfile="../patches/zram-ar71xx-tiny.patch"
 if ! patch -R -p1 -s -f --ignore-whitespace --dry-run <$patchfile; then
   patch -p1 --ignore-whitespace <$patchfile
 fi
-grep 'zram-swap'  targets/ar71xx-tiny b/targets/ar71xx-tiny
+grep 'zram-swap'  targets/ar71xx-tiny 
+
+cd openwrt 
+patchfile="../../patches/zram-ar71xx-tiny-kernel.patch"
+if ! patch -R -p1 -s -f --ignore-whitespace --dry-run <$patchfile; then
+  patch -p1 --ignore-whitespace <$patchfile
+fi
+grep 'CONFIG_SWAP'  target/linux/ar71xx/tiny/config-default
 
--- templates/17_wip/prepare.sh
+++ templates/17_wip/prepare.sh
@@ -10,6 +10,6 @@ cd ../gluon ; ../patches/fix-DIR615c1-imagetoobig.sh # remove DIR615C1 beeing to
 cd ../gluon ; ../patches/add-TPlinkArcherA7V5.sh # adding TP-Link Archer A7-V5
 cd ../gluon ; ../patches/zram-ar71xx-tiny.sh # enable zram for ar71xx tiny
 cd ../gluon ; ../patches/ignore-preservechannels-for-outdoormode.sh # correct handling of outdoor-devices (enable auto-channel)
-cd ../gluon ; ../patches/gluon-modules-down.sh # downdate kernel/packages of gluon/openwrt (OOM-mitigation attempt von 4/32 ath9k)
+#cd ../gluon ; ../patches/gluon-modules-down.sh # downdate kernel/packages of gluon/openwrt (OOM-mitigation attempt von 4/32 ath9k)
 exit 0;
 
