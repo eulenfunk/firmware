0fca80e225bbeb99edaf0cc12ea2fc9e19ea823e
Adorfer <adorferen@gmail.com>
2022-01-09 02:03:15 +0100

    updated xiami mi4agigabit backport

--- patches/add-mi-router-4a-gigabit.sh
+++ patches/add-mi-router-4a-gigabit.sh
@@ -15,3 +15,5 @@ fi
 grep  'mi-router-4a-gigabit' target/linux/ramips/image/mt7621.mk
 
 cp ../../patches/XIAOMI-MIR4A-GIGABIT.dts target/linux/ramips/dts/XIAOMI-MIR4A-GIGABIT.dts
+cp ../../patches/u-boot-upgrade target/linux/ramips/base-files/sbin/u-boot-upgrade
+cp ../../patches/690-net-add-support-for-threaded-NAPI-polling.patch target/linux/generic/pending-4.14/690-net-add-support-for-threaded-NAPI-polling.patch
